20d719246776978577601d6dc31c361d40b844b2
F4hdC <fhdwebdesign@gmail.com>
2021-06-12 11:17:10 +0200

    SomeModifications

---
+++ src/components/contacts/contacts.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contacts extends Component {
+    render() {
+        return (
+            <div>
+                <h1>My first Component "Contacts"</h1>
+                <ul>
+                    <li>Tel: 003322422</li>
+                    <li>Email: asdf@qwer.ty</li>
+                </ul> 
+            </div>
+        )
+    }
+}
+
+export default Contacts;
