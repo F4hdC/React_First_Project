a08da300cb56f22e4cfb710af67f3f41dccca952
F4hdC <fhdwebdesign@gmail.com>
2021-06-12 14:13:01 +0200

    SomeCss

--- src/components/contacts/contacts.js
+++ src/components/contacts/contacts.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-
+import './Contacts.css'
 
 // Para pasar los parametros a una clase, basta con llamarlo con this.props
 class Contacts extends Component {
     render() {
         const {name,tel,email} = this.props;
         return (
-            <div>
+            <div className="div_contacts">
                 <h3>Name of Contact: {name}</h3>
                 <ul>
                     <li>Tel: {tel}</li>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import  './Navbar.css';
 // Para pasar parametros a una funcion, hay que añadirlos dentro de los parentesis
 const Navbar = (parametros) => {
     // Creo un objeto para no repetir escribir parametros.....
     const { title } = parametros;
     return (
-        <div>
+        <div className="h1_navbar">
             <h1>{title}</h1>
         </div>
     )
