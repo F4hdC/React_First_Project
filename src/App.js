5c93b273e1c2f92e1320e48c7ec136ae0894fff0
F4hdC <fhdwebdesign@gmail.com>
2021-06-12 17:58:36 +0200

    AddBootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import Contacts from './components/contacts/Contacts';
 import Navbar from './components/navbar/Navbar';
 
+
 // Para pasar los parametros a una funcion como la Navbar
 function App() {
   return (
     <div className="App">
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        </header>*/}
-      <Navbar title="Title from Navbar: Contacts"/> 
-      <Navbar /> 
-      <Contacts />
+      <Navbar />
       <Contacts name="David" tel="00332211" email="desde@clase.es"/>
-      <Contacts name="Oscar" tel="02222222" email="desde2@clase.es"/>
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 import  './Navbar.css';
+
 // Para pasar parametros a una funcion, hay que añadirlos dentro de los parentesis
 const Navbar = (parametros) => {
     // Creo un objeto para no repetir escribir parametros.....
     const { title } = parametros;
     return (
-        <div className="h1_navbar">
-            <h1>{title}</h1>
-        </div>
+        <div className="container">
+        <header className="d-flex justify-content-center py-3">
+          <ul className="nav nav-pills">
+            <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
+            <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+          </ul>
+        </header>
+      </div>
     )
 }
 
